b6c12762bdc50d141a2cc0d89a87b73796ce2209
Arthur De Oliveira <arthurbruttyoliveira@gmail.com>
2026-09-22 20:20:37 -0300

    Add files via upload
    Exercícios para comparar valores e mostrar respostas diferentes dependendo da situação.

---
+++ Parte2-Condicionais/ex7.py
@@ -0,0 +1,9 @@
+numero = float(input("Digite um numero:"))
+numero2 = float(input("Digite outro numero:"))
+
+if numero > numero2: 
+    print("O maior numero e:", numero)
+elif numero2 > numero:
+    print("O maior numero e:", numero2)
+else:
+    print("Os numeros sao iguais")
---
+++ Parte2-Condicionais/ex9.py
@@ -0,0 +1,7 @@
+media = float(input("Digite a media do aluno: "))
+if media >= 7:
+    print("Aluno aprovado")
+elif media >= 5:
+    print("Aluno em recuperacao")
+else:
+    print("Aluno reprovado")
